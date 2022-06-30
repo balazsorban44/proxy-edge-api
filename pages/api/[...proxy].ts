014c7e6455651e59d112e219edaa08f6d8711d1e
Balázs Orbán <info@balazsorban.com>
2022-06-30 15:15:03 +0200

    try POST

--- pages/api/[...proxy].ts
+++ pages/api/[...proxy].ts
@@ -4,9 +4,13 @@ export default function handler(req: NextRequest) {
   if (req.headers.get("authorization") === "app-auth") {
     req.headers.set("authorization", "backend-auth")
   }
-
+  req.headers.delete("connection")
   const path = req.nextUrl.pathname.replace("/api", "/api/backend")
-  return fetch(`http://localhost:3000/${path}`, req)
+  return fetch(`http://localhost:3000/${path}`, {
+    headers: req.headers,
+    method: req.method,
+    body: req.body,
+  })
 }
 
 export const config = { runtime: "experimental-edge" }
